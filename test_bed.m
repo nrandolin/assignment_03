0c1d3fe6aa331ff9d8bc88e5af02cd857357ca53
nrandolin <nrand@olin.edu>
2024-10-06 20:42:45 -0400

    kinda fix the range thing
    not really though, steps are equal size but dont nessicarily reach the end

--- test_bed.m
+++ test_bed.m
@@ -1,9 +1,9 @@
 clear
 
 %% Test Forward Euler
-tspan = [0,5];
-X0 = [1; 0];
-h_ref = 0.01;
+tspan = [0,4.9];
+X0 = [0; 1];
+h_ref = 0.2;
 [t_list_01,X_list_01,h_avg, num_evals] = forward_euler_fixed_step_integration(@rate_func01,tspan,X0,h_ref);
 
 t = 0:0.001:5;
