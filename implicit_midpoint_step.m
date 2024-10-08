3dde44b6ac6505602ce0b008c5dd90a9e08f9b54
nrandolin <nrand@olin.edu>
2024-10-08 11:58:42 -0400

    Create implicit_midpoint_step.m

---
+++ implicit_midpoint_step.m
@@ -0,0 +1,16 @@
+%This function computes the value of X at the next time step
+%using the implicit midpoint approximation
+%INPUTS:
+%rate_func_in: the function used to compute dXdt. rate_func_in will
+% have the form: dXdt = rate_func_in(t,X) (t is before X)
+%t: the value of time at the current step
+%XA: the value of X(t)
+%h: the time increment for a single step i.e. delta_t = t_{n+1} - t_{n}
+%OUTPUTS:
+%XB: the approximate value for X(t+h) (the next step)
+% formula depends on the integration method used
+%num_evals: A count of the number of times that you called
+% rate_func_in when computing the next step
+function [XB,num_evals] = implicit_midpoint_step(rate_func_in,t,XA,h)
+    %your code here
+end
