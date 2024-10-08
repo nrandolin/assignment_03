a72b883d6fe660a6f67006e61072717d51500dee
lilydao11 <ldao@olin.edu>
2024-10-08 11:11:12 -0400

    trying

---
+++ backward_euler.m
@@ -0,0 +1,68 @@
+%This function computes the value of X at the next time step
+%using the Backward Euler approximation
+%INPUTS:
+%rate_func_in: the function used to compute dXdt. rate_func_in will
+% have the form: dXdt = rate_func_in(t,X) (t is before X)
+%t: the value of time at the current step
+%XA: the value of X(t)
+%h: the time increment for a single step i.e. delta_t = t_{n+1} - t_{n}
+%OUTPUTS:
+%XB: the approximate value for X(t+h) (the next step)
+% formula depends on the integration method used
+%num_evals: A count of the number of times that you called
+% rate_func_in when computing the next step
+function [XB,num_evals] = backward_euler(rate_func_in,t,XA,h)
+    function [f_val] = euler(rate_func_in,XA, t, h)
+        f_val = @(XB) XA + h*rate_func_in(t+h, XB) - XB
+    end
+    XB = multi_newton_solver(euler, XA, true)
+    num_evals = 1;
+end
+
+%% MULTI NEWTON SOLVER
+function x_next = multi_newton_solver(fun,x_guess,varargin)
+%true if supposed to use analytical jacobian, false otherwise
+use_analytical_jacobian = nargin==3 && varargin{1}(1);
+
+A_thresh = 10e-14;
+B_thresh = 10e-14;
+
+    if use_analytical_jacobian == true
+    f_val = fun(x_guess);
+    J = approximate_jacobian(fun,x_guess);
+    x_next = x_guess;
+        while max(abs(J\f_val))> A_thresh && max(abs(f_val))>B_thresh && abs(det(J*J')) > 1e-14
+            %calculate next x
+            x_next = x_next - J\f_val;
+            %establish next y value
+            f_val = fun(x_next);
+            J = approximate_jacobian(fun,x_next);      
+        end
+    end
+
+%Loop through until x is small
+    if use_analytical_jacobian == false
+    [f_val, J] = fun(x_guess);
+    x_next = x_guess;
+        while max(abs(J\f_val))> A_thresh && max(abs(f_val))>B_thresh && abs(det(J*J')) > 1e-14
+            %calculate next x
+            x_next = x_next - J\f_val;
+            %establish next y value
+            [f_val, J]  = fun(x_next);        
+        end
+    end
+
+end
+%% APPROX JACOBIAN
+function J = approximate_jacobian(fun,x)
+    % Set initial variables
+    ej = zeros(length(x), 1); %variable to store vector of multiplyers
+    h = 1e-6;
+    J = zeros(length(fun(x)), length(x));
+    for i = 1:size(J, 2)
+        ej(i) = 1;
+        % calculate the partial derivative 
+        J(:, i) = (fun(x+h*ej) - fun(x-h*ej))/(2*h);
+        ej(i) = 0;
+    end 
+end
