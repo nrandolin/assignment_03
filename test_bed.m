ce16daf1951c8f7394090cc667d4ed9d7f71dba8
lilydao11 <ldao@olin.edu>
2024-10-07 20:47:07 -0400

    testing p values for h changes

--- test_bed.m
+++ test_bed.m
@@ -1,3 +1,22 @@
+%% test local h/p val
+t = 0.5;
+h_list = linspace(10E-5,10,50);
+tspan = [0,t];
+X0 = [0; 1];
+local_error = [];
+difference = [];
+
+for i = 1:length(h_list)
+   h_ref = h_list(i);
+   % Calculate numperical x value
+   [t_list,X_list,h_avg, num_evals] = forward_euler_fixed_step_integration(@rate_func01,tspan,X0,h_ref);
+   X_numerical = X_list(end);
+   % calculate the real x value
+   X_true = solution01(t+h_ref);
+   local_error = [local_error, abs(X_numerical-X_true)];
+   difference = [difference, abs(solution01(t) - X_true)];
+end
+[p,k] = loglog_fit(h_list,local_error)
 %% Test iteration solver
 tspan = [0,4.9];
 h_ref = 0.2;
